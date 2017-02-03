71c171a6c93dbfe08f44bd3d95d6c024d6f19bd3
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2017-02-03 18:34:31 +0100

    Added copyright & license header to all source files

--- MacBPGViewer/BPGDocument.m
+++ MacBPGViewer/BPGDocument.m
@@ -1,10 +1,32 @@
-//
-//  Document.m
-//  MacBPGViewer
-//
-//  Created by Sveinbjorn Thordarson on 02/02/2017.
-//  Copyright © 2017 Sveinbjorn Thordarson. All rights reserved.
-//
+/*
+ MacBPGViewer - Native open-source BPG image viewer for OS X
+ Copyright (c) 2017, Sveinbjorn Thordarson <sveinbjornt@gmail.com>
+ 
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice, this
+ list of conditions and the following disclaimer in the documentation and/or other
+ materials provided with the distribution.
+ 
+ 3. Neither the name of the copyright holder nor the names of its contributors may
+ be used to endorse or promote products derived from this software without specific
+ prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #import "BPGDocument.h"
 #import "HCImage+BPG.h"
@@ -14,31 +36,13 @@
     IBOutlet NSImageView *imageView;
     NSImage *bpgImage;
 }
-
 @end
 
 @implementation BPGDocument
 
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        // Add your subclass-specific initialization here.
-    }
-    return self;
-}
-
 - (NSString *)windowNibName {
     return @"BPGDocument";
 }
-
-
-- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError {
-    // Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    [NSException raise:@"UnimplementedMethod" format:@"%@ is unimplemented", NSStringFromSelector(_cmd)];
-    return nil;
-}
-
 
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
     
@@ -46,6 +50,8 @@
         *outError = [NSError errorWithDomain:NSCocoaErrorDomain code:NSFileReadUnknownError userInfo:nil];
         return NO;
     }
+    
+    // TODO: add header magic validation
     
     bpgImage = [NSImage imageWithBPGData:data];
     if (bpgImage == nil) {
@@ -69,6 +75,5 @@
     
     [imageView setImage:bpgImage];
 }
-
 
 @end
